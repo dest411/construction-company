a728fb853e1f23526be5fa52c95c2e98391abd6c
dest411 <vadimstrizhak652@gmail.com>
2025-07-17 01:33:11 +0300

    completed js

--- main.js
+++ main.js
@@ -1,4 +1,24 @@
+// function togglemenu() {
+//     const menu = document.querySelector('.control-menu');
+//     menu.classList.toggle('open');
+// }
+
+
+
+
+
+const menu = document.querySelector('.control-menu');
+
 function togglemenu() {
-    const menu = document.querySelector('.control-menu');
-    menu.classList.toggle('open');
+  menu.classList.toggle('open');
 }
+document.addEventListener('click', (e) => {
+  const btn = document.querySelector('.menuimg');
+  if (
+    menu.classList.contains('open') &&
+    !menu.contains(e.target) &&
+    e.target !== btn
+  ) {
+    menu.classList.remove('open');
+  }
+});
